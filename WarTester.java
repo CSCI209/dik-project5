b04212f934e1be4d168e0c4f4e0f11229a9b5b76
wenz19 <32824280+wenz19@users.noreply.github.com>
2017-10-26 19:32:46 -0400

    Update WarTester.java

--- WarTester.java
+++ WarTester.java
@@ -1,7 +1,7 @@
 /**
  * A tester program for the WarGame model
  * @author Trevor Stalnaker
- * @editor Joe Wen, MacKenzie Leroy and Michael dik
+ * @editors Joe Wen, MacKenzie Leroy and Michael dik
  *
  */
 
